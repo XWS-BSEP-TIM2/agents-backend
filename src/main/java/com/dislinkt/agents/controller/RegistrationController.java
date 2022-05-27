8d3b59b5f919d4c78e30f54eb3765c7c187f5c6d
Tara Pogančev <tarapogancev@gmail.com>
2022-05-27 21:16:35 +0200

    feat: register

--- src/main/java/com/dislinkt/agents/controller/RegistrationController.java
+++ src/main/java/com/dislinkt/agents/controller/RegistrationController.java
@@ -4,8 +4,6 @@ import com.dislinkt.agents.dto.UserDTO;
 import com.dislinkt.agents.model.ApplicationUser;
 import com.dislinkt.agents.service.interfaces.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,10 +17,10 @@ public class RegistrationController {
     private final UserService userService;
 
     @PostMapping
-    public ResponseEntity<?> registerNewUser(@RequestBody UserDTO newUser) {
+    public boolean registerNewUser(@RequestBody UserDTO newUser) {
         ApplicationUser createdUser = userService.registerNewUser(newUser);
-        if (createdUser != null) return new ResponseEntity(createdUser.getFullName(), HttpStatus.OK);
-        else return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        if (createdUser != null) return true;
+        else return false;
     }
 
 }
