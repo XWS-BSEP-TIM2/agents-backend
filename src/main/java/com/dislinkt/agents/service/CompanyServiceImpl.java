df97f854db2d0268202058ba0e2ab3398b272aab
Tara Pogančev <tarapogancev@gmail.com>
2022-05-28 22:10:27 +0200

    feat: service layer

--- src/main/java/com/dislinkt/agents/service/CompanyServiceImpl.java
+++ src/main/java/com/dislinkt/agents/service/CompanyServiceImpl.java
@@ -3,10 +3,12 @@ package com.dislinkt.agents.service;
 import com.dislinkt.agents.dto.CompanyDTO;
 import com.dislinkt.agents.model.Company;
 import com.dislinkt.agents.service.interfaces.CompanyService;
+import com.dislinkt.agents.service.interfaces.ConverterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -14,6 +16,9 @@ public class CompanyServiceImpl implements CompanyService {
 
     @Autowired
     private MongoTemplate mongoTemplate;
+
+    @Autowired
+    private ConverterService converterService;
 
     @Override
     public Company getCompanyByUserId(String id) {
@@ -27,16 +32,39 @@ public class CompanyServiceImpl implements CompanyService {
 
     @Override
     public List<CompanyDTO> findAllDTO() {
-        return null;
+        List<CompanyDTO> retVal = new ArrayList<>();
+        for (Company company: mongoTemplate.findAll(Company.class)) {
+            retVal.add(converterService.companyToDto(company));
+        }
+        return retVal;
     }
 
     @Override
     public CompanyDTO findByIdDTO(String companyId) {
-        return null;
+        Company company = mongoTemplate.findById(companyId, Company.class);
+        if (company != null) {
+            return converterService.companyToDto(company);
+        } else {
+            return null;
+        }
     }
 
     @Override
     public boolean update(CompanyDTO company) {
-        return false;
+        Company companyToUpdate = mongoTemplate.findById(company.getId(), Company.class);
+        if (companyToUpdate != null) {
+            companyToUpdate.setName(company.name);
+            companyToUpdate.setDescription(company.description);
+            companyToUpdate.setEmailList(company.emailList);
+            companyToUpdate.setPhoneNumberList(company.phoneNumberList);
+            companyToUpdate.setTagline(company.tagline);
+            companyToUpdate.setTechnologies(company.technologies);
+            companyToUpdate = mongoTemplate.save(companyToUpdate);
+
+            return true;
+        } else {
+            return false;
+        }
+
     }
 }
--- src/main/java/com/dislinkt/agents/service/JobOfferServiceImpl.java
+++ src/main/java/com/dislinkt/agents/service/JobOfferServiceImpl.java
@@ -4,11 +4,15 @@ import com.dislinkt.agents.dto.JobOfferCommentDTO;
 import com.dislinkt.agents.dto.JobOfferDTO;
 import com.dislinkt.agents.model.JobOffer;
 import com.dislinkt.agents.model.JobOfferComment;
+import com.dislinkt.agents.model.Post;
+import com.dislinkt.agents.model.enums.PostType;
+import com.dislinkt.agents.service.interfaces.ConverterService;
 import com.dislinkt.agents.service.interfaces.JobOfferService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -17,38 +21,106 @@ public class JobOfferServiceImpl implements JobOfferService {
     @Autowired
     private MongoTemplate mongoTemplate;
 
+    @Autowired
+    private ConverterService converterService;
+
     @Override
     public List<JobOfferDTO> findAllDTO() {
-        return null;
+        List<JobOfferDTO> retVal = new ArrayList<>();
+        for (JobOffer jobOffer: mongoTemplate.findAll(JobOffer.class)) {
+            retVal.add(converterService.jobOfferToDto(jobOffer));
+        }
+        return retVal;
     }
 
     @Override
     public JobOfferDTO findByIdDTO(String offerId) {
-        return null;
+        JobOffer jobOffer = mongoTemplate.findById(offerId, JobOffer.class);
+        if (jobOffer != null) {
+            return converterService.jobOfferToDto(jobOffer);
+        } else {
+            return null;
+        }
     }
 
     @Override
     public List<JobOfferCommentDTO> findCommentsByIdDTO(String offerId) {
-        return null;
+        List<JobOfferCommentDTO> retVal = new ArrayList<>();
+        for (JobOfferComment comment: mongoTemplate.findAll(JobOfferComment.class)) {
+            if (comment.getJobOfferId().equals(offerId)) {
+                retVal.add(converterService.commentToDto(comment));
+            }
+        }
+        return retVal;
     }
 
     @Override
     public JobOffer postNewOffer(JobOfferDTO jobOffer) {
-        return null;
+        JobOffer newOffer = new JobOffer();
+        newOffer.setCompanyId(jobOffer.company.id);
+        newOffer.setDescription(jobOffer.getDescription());
+        newOffer.setPosition(jobOffer.getPosition());
+        newOffer.setTechnologies(jobOffer.getTechnologies());
+        newOffer.setSeniority(jobOffer.getSeniority());
+        newOffer.setPosition(jobOffer.getPosition());
+        newOffer.setUserId(jobOffer.company.user.id);
+        newOffer = mongoTemplate.save(newOffer);
+
+        Post post = new Post();
+        post.setJobOfferId(newOffer.getId());
+        post.setPostType(PostType.NEW_JOB_OFFER);
+        mongoTemplate.save(post);
+
+        return newOffer;
     }
 
     @Override
     public JobOfferComment postNewComment(JobOfferCommentDTO comment) {
-        return null;
+        JobOfferComment jobOfferComment = new JobOfferComment();
+        jobOfferComment.setComment(comment.getComment());
+        jobOfferComment.setJobOfferId(comment.getJobOffer().id);
+        jobOfferComment.setRating(comment.getRating());
+        jobOfferComment.setSalary(comment.getSalary());
+        jobOfferComment.setUserId(comment.getUser().id);
+        return mongoTemplate.save(jobOfferComment);
     }
 
     @Override
     public JobOffer updateOffer(JobOfferDTO jobOffer) {
-        return null;
+        JobOffer updatedOffer = mongoTemplate.findById(jobOffer.id, JobOffer.class);
+        if (updatedOffer != null) {
+            updatedOffer.setDescription(jobOffer.getDescription());
+            updatedOffer.setPosition(jobOffer.getPosition());
+            updatedOffer.setTechnologies(jobOffer.getTechnologies());
+            updatedOffer.setSeniority(jobOffer.getSeniority());
+            updatedOffer.setPosition(jobOffer.getPosition());
+            updatedOffer = mongoTemplate.save(updatedOffer);
+        }
+
+        return updatedOffer;
     }
 
     @Override
     public boolean deleteOffer(JobOfferDTO jobOffer) {
-        return false;
+        JobOffer offer = mongoTemplate.findById(jobOffer.id, JobOffer.class);
+        if (offer != null) {
+            for (Post post: mongoTemplate.findAll(Post.class)) {
+                if (post.getJobOfferId().equals(offer.getId())) {
+                    mongoTemplate.remove(post);
+                }
+            }
+
+            for (JobOfferComment comment: mongoTemplate.findAll(JobOfferComment.class)) {
+                if (comment.getJobOfferId().equals(offer.getId())) {
+                    mongoTemplate.remove(comment);
+                }
+            }
+
+            mongoTemplate.remove(offer);
+
+            return true;
+        } else {
+            return false;
+        }
     }
 }
