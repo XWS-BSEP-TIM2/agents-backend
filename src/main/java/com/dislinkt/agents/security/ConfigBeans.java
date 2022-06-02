b4f6f6d53ae03da02889b0f49d2cf4caf1494154
Tara Pogančev <tarapogancev@gmail.com>
2022-06-02 10:27:55 +0200

    fix: port changed to 4201

--- src/main/java/com/dislinkt/agents/security/ConfigBeans.java
+++ src/main/java/com/dislinkt/agents/security/ConfigBeans.java
@@ -13,7 +13,7 @@ public class ConfigBeans {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("**").allowedOrigins("http://localhost:4200");
+                registry.addMapping("**").allowedOrigins("http://localhost:4201");
             }
         };
     }
