831525ad56121ada8d7437fe417fb1c52178899e
ljuti21 <prodanovic.ra34.2018@uns.ac.rs>
2022-06-11 12:37:37 +0200

    feat: added logging to rest of the controllers
    Where it needed.

--- src/main/java/com/dislinkt/agents/controller/JobOfferController.java
+++ src/main/java/com/dislinkt/agents/controller/JobOfferController.java
@@ -4,6 +4,7 @@ import com.dislinkt.agents.dto.JobOfferCommentDTO;
 import com.dislinkt.agents.dto.JobOfferDTO;
 import com.dislinkt.agents.model.JobOffer;
 import com.dislinkt.agents.model.JobOfferComment;
+import com.dislinkt.agents.service.LoggingService;
 import com.dislinkt.agents.service.interfaces.JobOfferService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 public class JobOfferController {
 
     private final JobOfferService jobOfferService;
+    private final LoggingService loggingService;
 
     @PostMapping
     @PreAuthorize("hasRole('COMPANY_OWNER')")
@@ -25,18 +27,21 @@ public class JobOfferController {
     @PostMapping("/comment")
     @PreAuthorize("hasRole('USER')")
     public JobOfferComment postNewOfferComment(@RequestBody JobOfferCommentDTO comment) {
+        loggingService.MakeInfoLog("User "+ comment.user.getEmail() + " made a comment.");
         return jobOfferService.postNewComment(comment);
     }
 
     @PutMapping
     @PreAuthorize("hasRole('COMPANY_OWNER')")
     public JobOffer updateOffer(@RequestBody JobOfferDTO jobOffer) {
+        loggingService.MakeInfoLog("Owner of "+ jobOffer.getCompany().getName() + " company made a offer.");
         return jobOfferService.updateOffer(jobOffer);
     }
 
     @PutMapping("/delete")
     @PreAuthorize("hasRole('COMPANY_OWNER')")
     public boolean deleteOffer(@RequestBody JobOfferDTO jobOffer) {
+        loggingService.MakeInfoLog("Owner of "+ jobOffer.getCompany().getName() + " company deleted a offer.");
         return jobOfferService.deleteOffer(jobOffer);
     }
 
